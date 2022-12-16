21c3fd6c48ce1d883730366693cdf28aa4d2bd14
Asael <aatello@pichincha.com>
2022-12-16 13:48:07 -0500

    feat: added spec for empty list

--- src/components/pages/main/gift-container/gift-container.spec.tsx
+++ src/components/pages/main/gift-container/gift-container.spec.tsx
@@ -2,9 +2,10 @@ import { render, fireEvent } from '@testing-library/react-native';
 import { GiftContainer } from './gift-container';
 
 describe('Gift Container Suite', () => {
-  xit('Should render with empty gift list ', () => {
-    const { getByText } = render(<GiftContainer />)
-    const addButton = getByText("ADD")
-    expect(addButton).toBeDefined();
+  it('Should render with empty gift list ', () => {
+    const { debug,getByText } = render(<GiftContainer />)
+    debug();
+    const text = getByText("No posee gifts")
+    expect(text).toBeTruthy();
   });
 });
