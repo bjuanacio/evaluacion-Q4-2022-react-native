a71c17ac2269a8f9cc7811e0fd93d276cbd5384b
Asael <aatello@pichincha.com>
2022-12-16 13:15:15 -0500

    refactor: removed non-used files

--- src/components/pages/main/main.test.tsx
+++
@@ -1,7 +0,0 @@
-import {render, screen, fireEvent} from '@testing-library/react-native';
-import {primary, background} from '../../../utils/theme/theme';
-
-
-describe('Main Component', () => {
-  
-});
