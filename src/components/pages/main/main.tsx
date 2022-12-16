0058ccabcc1d2c2a550a4f37ca7078f5b653e0a8
Asael <aatello@pichincha.com>
2022-12-16 11:06:27 -0500

    refactor: removed non-used imports

--- src/components/pages/main/main.tsx
+++ src/components/pages/main/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { GiftProvider } from '../../../context/gift-context/gift-context';
 import { GiftList } from './gift-list/gift-list';
-import WarningIcon from '../../../assets/warning-icon.png';
-// import Icon from '../../atoms/icon/icon';
 
 export const Main = () => {
   return (
